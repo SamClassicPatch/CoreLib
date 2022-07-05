54ed7ce78bde6400cbdf34410f7aef4c9f7f2884
Dreamy Cecil <stalker27production@gmail.com>
2022-07-05 05:22:12 +0300

    Move patch API & pointer to CGame from GameExecutable to CoreLib.

--- Core.cpp
+++ Core.cpp
@@ -13,19 +13,7 @@ You should have received a copy of the GNU General Public License along
 with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
 
-// Main engine components
-#include <Engine/Engine.h>
-#include <Engine/CurrentVersion.h>
-#include "Game/Game.h"
-
-// Import library for use
-#ifndef CORE_EXPORTS
-  #pragma comment(lib, "Core.lib")
-#endif
+#include "StdH.h"
 
 // Pointer to the Game module
-extern CGame *_pGame;
-
-// Common components
-#include "API/PatchAPI.h"
-#include "FuncPatching.h"
+CGame *_pGame = NULL;
