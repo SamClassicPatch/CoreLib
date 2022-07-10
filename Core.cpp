ddbf8c78618fcaef62e425c035443b284fcef5ef
Dreamy Cecil <stalker27production@gmail.com>
2022-07-10 03:19:11 +0300

    Load engine plugins for Core.

--- Core.cpp
+++ Core.cpp
@@ -63,4 +63,7 @@ void CECIL_InitCore(void) {
   _pShell->DeclareSymbol("           user CTString sam_strFirstLevel;", &sam_strFirstLevel);
   _pShell->DeclareSymbol("persistent user CTString sam_strIntroLevel;", &sam_strIntroLevel);
   _pShell->DeclareSymbol("persistent user CTString sam_strGameName;",   &sam_strGameName);
+
+  // Load Core plugins
+  _pCoreAPI->LoadPlugins(CPluginAPI::PF_ENGINE);
 };
