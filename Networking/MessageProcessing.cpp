9900be71cb31e2844d2e8479dc1384137ae53a8a
Dreamy Cecil <stalker27production@gmail.com>
2022-12-18 22:10:24 +0300

    Process MSG_REQ_CONNECTPLAYER packet manually via the server handler.

--- Networking/MessageProcessing.cpp
+++ Networking/MessageProcessing.cpp
@@ -16,7 +16,11 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "StdH.h"
 
 #include "MessageProcessing.h"
+#include "NetworkFunctions.h"
 #include "Modules.h"
+
+#include "Interfaces/DataFunctions.h"
+#include "Query/QueryManager.h"
 
 // Client requesting the session state
 BOOL OnConnectRemoteSessionStateRequest(INDEX iClient, CNetworkMessage &nmMessage)
@@ -32,7 +36,78 @@ BOOL OnConnectRemoteSessionStateRequest(INDEX iClient, CNetworkMessage &nmMessag
 // Client requesting the connection to the server
 BOOL OnPlayerConnectRequest(INDEX iClient, CNetworkMessage &nmMessage)
 {
-  return TRUE;
+  // Read character data
+  CPlayerCharacter pcCharacter;
+  nmMessage >> pcCharacter;
+
+  static CSymbolPtr pstrNameMask("ser_strNameMask");
+  static CSymbolPtr pbWhiteList("ser_bInverseBanning");
+
+  // Character name is banned
+  if (IData::MatchesMask(pcCharacter.GetName(), pstrNameMask.GetString()) == !pbWhiteList.GetIndex()) {
+    INetwork::SendDisconnectMessage(iClient, TRANS("You are banned from this server"), TRUE);
+    return FALSE;
+  }
+
+  CServer &srv = _pNetwork->ga_srvServer;
+  CSessionSocket &sso = srv.srv_assoSessions[iClient];
+
+  // Check if someone's connecting with too many players
+  if (iClient > 0 && INetwork::CountClientPlayers(iClient) >= sso.sso_ctLocalPlayers) {
+    INetwork::SendDisconnectMessage(iClient, TRANS("Protocol violation"), FALSE);
+    return FALSE;
+  }
+
+  // Find inactive player for the client
+  CPlayerBuffer *pplbNew = INetwork::FirstInactivePlayer();
+
+  // If there's a used character already
+  if (INetwork::IsCharacterUsed(pcCharacter)) {
+    // Refuse connection
+    CTString strMessage;
+    strMessage.PrintF(TRANS("Player character '%s' already exists in this session."), pcCharacter.GetName());
+
+    INetwork::SendDisconnectMessage(iClient, strMessage, FALSE);
+
+  // If able to add new players
+  } else if (pplbNew != NULL) {
+    // Activate new player
+    pplbNew->Activate(iClient);
+    INDEX iNewPlayer = pplbNew->plb_Index;
+
+    // Remember the character
+    pplbNew->plb_pcCharacter = pcCharacter;
+
+    // Send message to all clients about adding a new player
+    CNetStreamBlock nsbAddClientData(MSG_SEQ_ADDPLAYER, ++srv.srv_iLastProcessedSequence);
+    nsbAddClientData << iNewPlayer;
+    nsbAddClientData << pcCharacter;
+
+    INetwork::AddBlockToAllSessions(nsbAddClientData);
+
+    // Don't wait for any more players
+    _pShell->Execute("ser_bWaitFirstPlayer = 0;");
+
+    // Reply to this client about adding a new player
+    CNetworkMessage nmPlayerRegistered(MSG_REP_CONNECTPLAYER);
+    nmPlayerRegistered << iNewPlayer;
+
+    _pNetwork->SendToClientReliable(iClient, nmPlayerRegistered);
+
+    // Notify master server that a player is connecting
+    static CSymbolPtr symptr("ser_bEnumeration");
+
+    if (symptr.GetIndex()) {
+      IMasterServer::OnServerStateChanged();
+    }
+
+  // If too many players
+  } else {
+    // Refuse connection
+    INetwork::SendDisconnectMessage(iClient, TRANS("Too many players in session."), FALSE);
+  }
+
+  return FALSE;
 };
 
 // Client changing the character
