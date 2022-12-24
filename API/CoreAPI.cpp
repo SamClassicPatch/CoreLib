0f2737f709bbdeb57afa80b3da2e90994326a602
Dreamy Cecil <stalker27production@gmail.com>
2022-12-24 12:31:47 +0300

    Bump version to 1.4.0.

--- API/CoreAPI.cpp
+++ API/CoreAPI.cpp
@@ -41,7 +41,7 @@ CCoreAPI::CCoreAPI() :
   ssNew.ss_pPreFunc = NULL; // Unused
   ssNew.ss_pPostFunc = NULL; // Unused
 
-  ulVersion = MakeVersion(1, 3, 1);
+  ulVersion = MakeVersion(1, 4, 0);
 
   // Output patcher actions
   if (FileExists(_fnmApplicationExe.FileDir() + "PatcherOutput")) {
