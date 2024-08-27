000bf267b47bce2dba29e9bf2071f613d47262ac
Dreamy Cecil <21009796+DreamyCecil@users.noreply.github.com>
2024-08-27 07:10:51 +0300

    Declare new "ClassicsPatchAPI" shell symbol for the reworked API.

--- API/ICore.cpp
+++ API/ICore.cpp
@@ -275,13 +275,17 @@ static void IncludeScript(SHELL_FUNC_ARGS) {
 
 void Core(void) {
   // Add API to symbols
-  CShellSymbol *pssNew = _pShell->sh_assSymbols.New();
-  pssNew->ss_strName = "CoreAPI";
-  pssNew->ss_pvValue = ClassicsPatchAPI(); // Pointer to the interface
-  pssNew->ss_istType = 0; // Should be '_shell_istUndeclared'
-  pssNew->ss_ulFlags = SSF_CONSTANT; // Unchangable
-  pssNew->ss_pPreFunc = NULL; // Unused
-  pssNew->ss_pPostFunc = NULL; // Unused
+  CShellSymbol *aNew = _pShell->sh_assSymbols.New(2);
+  aNew[0].ss_strName = "CoreAPI"; // Old symbol for compatibility
+  aNew[1].ss_strName = "ClassicsPatchAPI"; // New symbol signifying new API
+
+  for (INDEX i = 0; i < 2; i++) {
+    aNew[i].ss_pvValue = ClassicsPatchAPI(); // Pointer to the interface
+    aNew[i].ss_istType = 0; // Should be '_shell_istUndeclared'
+    aNew[i].ss_ulFlags = SSF_CONSTANT; // Unchangable
+    aNew[i].ss_pPreFunc = NULL; // Unused
+    aNew[i].ss_pPostFunc = NULL; // Unused
+  }
 
   // Information about the patch
   _pShell->DeclareSymbol("user void PatchInfo(void);", &PatchInfo);
